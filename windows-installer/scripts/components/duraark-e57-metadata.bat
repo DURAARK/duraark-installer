5ecc25bfb97dc44dd5a87ceb530b556a9627d4cd
unknown <CGV@MaxPayne.cgv.tugraz.at>
2015-10-13 10:58:11 +0200

    added scripts for 'duraark-pc2bim' and 'duraark-e57-metadata' components

---
+++ windows-installer/scripts/components/duraark-e57-metadata.bat
@@ -0,0 +1,7 @@
+@echo off
+"C:\Program Files\Docker Toolbox\docker-machine" env default --shell cmd > %TEMP%/set-docker-env.bat
+call %TEMP%\set-docker-env.bat
+del %TEMP%\set-docker-env.bat
+
+@echo on
+"C:\Program Files\Docker Toolbox\docker.exe" run --rm --name duraark-e57-metadata -v /c/Users/duraark-storage:/duraark-storage paulhilbert/e57-metadata e57_metadata_extractor --input /duraark-storage/%1 --output /duraark-storage/%1.json
---
+++ windows-installer/scripts/components/duraark-pc2bim.bat
@@ -0,0 +1,7 @@
+@echo off
+"C:\Program Files\Docker Toolbox\docker-machine" env default --shell cmd > %TEMP%/set-docker-env.bat
+call %TEMP%\set-docker-env.bat
+del %TEMP%\set-docker-env.bat
+
+@echo on
+"C:\Program Files\Docker Toolbox\docker.exe" run --rm --name duraark-pc2bim -v /c/Users/duraark-storage:/duraark-storage ubo/pc2bim pc2bim --input /duraark-storage/%1 --output /duraark-storage/%1.ifc
