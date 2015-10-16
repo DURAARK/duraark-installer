afa98ca93f759708d42f780930c6aa1ee39f136c
Martin Hecher <martinhecher@users.noreply.github.com>
2015-10-16 15:19:08 +0200

    Update 'duraark-pc2bim.bat' to additionally take a second argument to specify output filen ame

--- windows-installer/scripts/components/duraark-pc2bim.bat
+++ windows-installer/scripts/components/duraark-pc2bim.bat
@@ -4,4 +4,4 @@ call %TEMP%\set-docker-env.bat
 del %TEMP%\set-docker-env.bat
 
 @echo on
-"C:\Program Files\Docker Toolbox\docker.exe" run --rm --name duraark-pc2bim -v /c/Users/duraark-storage:/duraark-storage ubo/pc2bim pc2bim --input /duraark-storage/%1 --output /duraark-storage/%1.ifc
+"C:\Program Files\Docker Toolbox\docker.exe" run --rm --name duraark-pc2bim -v /c/Users/duraark-storage:/duraark-storage ubo/pc2bim pc2bim --input /duraark-storage/%1 --output /duraark-storage/%2
